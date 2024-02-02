585c204b0d646c89c6c4d2a80678dd4d20b5b5d6
basma <besma.bakirci01@gmail.com>
2024-02-02 22:52:48 +0300

    initial commit

--- src/Practice/practice1.java
+++ src/Practice/practice1.java
@@ -12,8 +12,8 @@ public class practice1 {
         //
         String My_Name = " Besma";
         String My_Surname= " Bakirci";
-        System.out.printf(" My Name is: %s\n My Surname is: %s\n", My_Name , My_Surname);
-        System.out.println(" Pov : Could you spell your name please!\n");
+        System.out.printf(" My Name is: %s\n My Surname is: %s \n", My_Name , My_Surname);
+        System.out.println(" Pov : Could you spell your name please! \n");
         System.out.println(" Ok!");
         Character MyChar1 = 'B';
         Character MyChar2 = 'E';
@@ -22,19 +22,22 @@ public class practice1 {
         Character MyChar5 = 'A';
         System.out.printf(" %c\n %c\n %c\n %c\n %c\n", MyChar1 , MyChar2 , MyChar3 , MyChar4 , MyChar5 );
         System.out.println("   ˶ᵔ ᵕ ᵔ˶  ");
-        System.out.println(" What subject did you search about recently?");
-        int secund = 1;
-        int minutes = 60 * secund;
-        int Screen_time_on_Socaial_Media_USA = 134 * 60 *secund;
+        System.out.println(" What subjects did you search about recently? \n");
+        System.out.print(" Example from USA about the waste time in life! \n");
+        int second = 1;
+        int minutes = 60 * second;
+        int Screen_time_on_Socaial_Media_USA = 134 * 60 ;
         int day = 1440 * minutes;
+        int month = 30 * day;
         int year = 365 * day;
         double Average_Years_of_Human_life = +70 * year;
+        float Sleeping_time_in_USA = (28 * year) + (2 * month) + (5 * day) ;
+        float population_USA = 334_914_895 * Sleeping_time_in_USA;
+        System.out.printf(" The total is: %.2f\n ", Average_Years_of_Human_life);
         System.out.printf(" The Total Screen Time On Socaial Media USA: %d \n ",Screen_time_on_Socaial_Media_USA );
-        System.out.printf(" The total is: %.2f \n ", Average_Years_of_Human_life);
-        System.out.print(" Don't waste your life! \n");
+        System.out.printf("spent time by sleeping %2f",population_USA);
         System.out.print(" Be careful that it's much time please invest it \n ");
-
-
+        System.out.println("THANK YOU FOR READING THIS CODE");
 
     }
 
