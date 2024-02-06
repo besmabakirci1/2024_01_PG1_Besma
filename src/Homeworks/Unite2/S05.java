354d88efa4c9b5259dd3fe0641d1d8ac2a4ba52e
basmabakirci1 <bessomehelwe@gmail.com>
2024-02-06 03:29:38 +0300

    initial commit

--- src/Homeworks/Unite2/S05.java
+++ src/Homeworks/Unite2/S05.java
@@ -1,4 +1,7 @@
 package Homeworks.Unite2;
+
+import java.util.Scanner;
+
 //(Financial application: calculate tips)
 //Write a program that reads the subtotal and the gratuity rate, then computes the gratuity and total.
 // For example: if the user enters 10 for subtotal and 15% for gratuity rate, the program displays $1.5 as gratuity and $11.5 as total.
@@ -6,4 +9,10 @@ package Homeworks.Unite2;
 //Enter the subtotal and a gratuity rate: 10 15-> "Enter"
 //The gratuity is $1.5 and total is $11.5
 public class S05 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println();
+    }
 }
