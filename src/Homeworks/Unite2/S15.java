086ff080ddac29c257027bc67dbeecf1f7b711da
basmabakirci1 <bessomehelwe@gmail.com>
2024-02-10 00:56:40 +0300

    initial commit

--- src/Homeworks/Unite2/S15.java
+++ src/Homeworks/Unite2/S15.java
@@ -1,4 +1,7 @@
 package Homeworks.Unite2;
+
+import java.util.Scanner;
+
 // (Geometry: Distance of Two Points)
 // This program calculates the distance between two points (x1, y1) and (x2, y2) entered by the user.
 // The distance formula is √((x2 - x1)^2 + (y2 - y1)^2).
@@ -7,4 +10,20 @@ package Homeworks.Unite2;
 //Enter x2 and y2: 4 5-> "Enter"
 //The distance between the two points is 8.764131445842194
 public class S15 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter x1 : ");
+        double x1 = input.nextDouble();
+        System.out.print("Please enter y1 : ");
+        double y1 = input.nextDouble();
+        System.out.print("Please enter x2 : ");
+        double x2 = input.nextDouble();
+        System.out.print("Please enter y2 : ");
+        double y2 = input.nextDouble();
+        input.close();
+
+        double distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+
+        System.out.println("The distance between the two points is: " + distance);
+    }
 }
--- src/Homeworks/Unite2/S18.java
+++ src/Homeworks/Unite2/S18.java
@@ -7,5 +7,24 @@ package Homeworks.Unite2;
 //  3 4 81
 //  4 5 1024
 //  5 6 15625
+
+import java.util.Scanner;
 public class S18 {
+    public static void main(String[] args) {
+        System.out.println("a\tb\tpow(a, b)");
+
+        int a1 = 1, a2 = 2, a3 = 3, a4 = 4, a5 = 5;
+        int b1 = 2, b2 = 3, b3 = 4, b4 = 5, b5 = 6;
+        int result1 = (int) Math.pow(a1, b1);
+        int result2 = (int) Math.pow(a2, b2);
+        int result3 = (int) Math.pow(a3, b3);
+        int result4 = (int) Math.pow(a4, b4);
+        int result5 = (int) Math.pow(a5, b5);
+
+        System.out.println(a1 + "\t" + b1 + "\t" + result1);
+        System.out.println(a2 + "\t" + b2 + "\t" + result2);
+        System.out.println(a3 + "\t" + b3 + "\t" + result3);
+        System.out.println(a4 + "\t" + b4 + "\t" + result4);
+        System.out.println(a5 + "\t" + b5 + "\t" + result5);
+    }
 }
