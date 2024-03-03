db5c5d88dec18ead5110a16372a8fc786b95142f
Basma Bakirci <144848782+besmabakirci1@users.noreply.github.com>
2024-03-03 14:30:07 +0300

    Initial commit

--- src/homework/unite3/S06.java
+++ src/homework/unite3/S06.java
@@ -1,5 +1,7 @@
 package homework.unite3;
 
+import java.util.Scanner;
+
 // (Health application: BMI)
 // Revise Listing 3.4, ComputeAndInterpretBMI.java, to let the user enter weight, feet, and inches.
 // For example, if a person is 5 feet and 10 inches, you will enter 5 for feet and 10 for inches.
@@ -10,7 +12,33 @@ package homework.unite3;
 // BMI is 20.087702275404553
 // Normal
 public class S06 {
-    public static void main(String[] args) {
-        System.out.println();
+
+    public static void main(String[] args){
+        Scanner input = new Scanner (System.in);
+        System.out.print("Enter weight in pounds: ");
+        double weight = input.nextDouble();
+        System.out.print("Enter inches: ");
+        double inches = input.nextDouble();
+        System.out.print("Enter feet: ");
+        double feet = input.nextDouble();
+
+        final double KILOGRAMS_PER_POUND = 0.45359237;
+        final double METERS_PER_INCH = 0.0254;
+
+        double totalInches = feet * 12 + inches;
+        double heightInMeters = totalInches * METERS_PER_INCH;
+        double weightInKilograms = weight * KILOGRAMS_PER_POUND;
+        double bmi = weightInKilograms / (heightInMeters * heightInMeters);
+
+        System.out.println("BMI is " +bmi);
+        if (bmi < 18.5)
+            System.out.println("Underweight");
+        else if (bmi < 25)
+            System.out.println("Normal");
+        else if (bmi < 30)
+            System.out.println("'Overweight");
+        else
+            System.out.println("Obese");
+
     }
 }
