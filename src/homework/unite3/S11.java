a9fca5a026296504ca463661008cfab30c9c3809
Basma Bakirci <144848782+besmabakirci1@users.noreply.github.com>
2024-03-03 22:23:09 +0300

    Initial commit

--- src/homework/unite3/S11.java
+++ src/homework/unite3/S11.java
@@ -1,9 +1,76 @@
 package homework.unite3;
 
+import java.util.Scanner;
+
 // (Find the number of days in a month)
 // Write a program that prompts the user to enter the month and year and displays the number of days in the month.
 // For example, if the user entered month 2 and year 2012, the program should display that February 2012 had 29 days.
 // If the user entered month 3 and year 2015, the program should display that March 2015 had 31 days.
 public class S11 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number_Of_DaysInMonth = 0;
+        String MonthOfName = "Unknown";
+
+        System.out.println("Please enter the month: ");
+        int month = input.nextInt();
+        System.out.println("Please enter the year: ");
+        int year = input.nextInt();
+
+        switch (month) {
+            case 1:
+                MonthOfName = "January";
+                number_Of_DaysInMonth = 31;
+                break;
+            case 2:
+                MonthOfName = "February";
+                if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))) {
+                    number_Of_DaysInMonth = 29;
+                } else {
+                    number_Of_DaysInMonth = 28;
+                }
+                break;
+            case 3:
+                MonthOfName = "March";
+                number_Of_DaysInMonth = 31;
+                break;
+            case 4:
+                MonthOfName = "April";
+                number_Of_DaysInMonth = 30;
+                break;
+            case 5:
+                MonthOfName = "May";
+                number_Of_DaysInMonth = 31;
+                break;
+            case 6:
+                MonthOfName = "June";
+                number_Of_DaysInMonth = 30;
+                break;
+            case 7:
+                MonthOfName = "July";
+                number_Of_DaysInMonth = 31;
+                break;
+            case 8:
+                MonthOfName = "August";
+                number_Of_DaysInMonth = 31;
+                break;
+            case 9:
+                MonthOfName = "September";
+                number_Of_DaysInMonth = 30;
+                break;
+            case 10:
+                MonthOfName = "October";
+                number_Of_DaysInMonth = 31;
+                break;
+            case 11:
+                MonthOfName = "November";
+                number_Of_DaysInMonth = 30;
+                break;
+            case 12:
+                MonthOfName = "December";
+                number_Of_DaysInMonth = 31;
+        }
+        System.out.print(MonthOfName + " " + year + " has " + number_Of_DaysInMonth + " days\n");
+    }
 
 }
