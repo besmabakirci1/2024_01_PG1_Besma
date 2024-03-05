e5ba27a4bd1da43ee2bcdb1bab4cc6893b40b008
Basma Bakirci <144848782+besmabakirci1@users.noreply.github.com>
2024-03-05 17:30:51 +0300

    Initial commit

--- src/homework/unite3/S16.java
+++ src/homework/unite3/S16.java
@@ -3,8 +3,14 @@ package homework.unite3;
 // (Random point)
 // Write a program that displays a random coordinate in a rectangle.
 // The rectangle is centered at (0, 0) with width 100 and height 200.
-public class S16 {
+public class S16{
     public static void main(String[] args) {
-        System.out.println();
+        int width = (int)((Math.random() * 100) );
+        int height = (int)((Math.random() * 200) );
+
+        System.out.println("Random coordinate in rectangle centered at (0,0)");
+        System.out.println("with width 100 and height 200: (" + width + ", " + height + ")");
     }
 }
+
+// kafam burda karıştı *100) -50; yapsaydım ne değişirdi veya *200) -100;
