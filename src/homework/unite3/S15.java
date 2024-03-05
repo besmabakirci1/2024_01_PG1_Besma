c3b186c13fb153a82dbb04de8afa4336fab8768d
Basma Bakirci <144848782+besmabakirci1@users.noreply.github.com>
2024-03-05 16:14:50 +0300

    Initial commit

--- src/homework/unite3/S15.java
+++ src/homework/unite3/S15.java
@@ -1,4 +1,5 @@
 package homework.unite3;
+import java.util.Scanner;
 
 // (Game: lottery) Revise Listing 3.8, Lottery.java, to generate a lottery of a three-digit number.
 // The program prompts the user to enter a three-digit number and determines whether the user wins according to the following rules:
@@ -7,6 +8,38 @@ package homework.unite3;
 // 3. If one digit in the user input matches a digit in the lottery number, the award is $1,000.
 public class S15 {
     public static void main(String[] args) {
-        System.out.println();
+        Scanner input = new Scanner(System.in);
+        int guess;
+
+        while (true) {
+            System.out.print("Enter your lottery pick (three digits): ");
+            guess = input.nextInt();
+            if (guess >= 100 && guess <= 999) {
+                break;
+            }
+            System.out.println("Please enter a three-digit number.");
+        }
+        int lottery = (int)(Math.random() * 1000);
+
+        int lotteryDigit1 = lottery / 100;
+        int lotteryDigit2 = (lottery / 10) % 10;
+        int lotteryDigit3 = lottery % 10;
+
+        int guessDigit1 = guess / 100;
+        int guessDigit2 = (guess / 10) % 10;
+        int guessDigit3 = guess % 10;
+
+        System.out.println("The lottery number is " + lottery);
+
+        if (guess == lottery)
+            System.out.println("Exact match: you win $10,000");
+        else if ((guessDigit1 == lotteryDigit1 || guessDigit1 == lotteryDigit2 || guessDigit1 == lotteryDigit3) &&
+                (guessDigit2 == lotteryDigit1 || guessDigit2 == lotteryDigit2 || guessDigit2 == lotteryDigit3) &&
+                (guessDigit3 == lotteryDigit1 || guessDigit3 == lotteryDigit2 || guessDigit3 == lotteryDigit3))
+            System.out.println("Match all digits: you win $3,000");
+        else if (guessDigit1 == lotteryDigit1 || guessDigit2 == lotteryDigit2 || guessDigit3 == lotteryDigit3)
+            System.out.println("Match one digit: you win $1,000");
+        else
+            System.out.println("Sorry, no match");
     }
 }
