3ffd159a5f944009c648cab89f08ca6a17f11b99
Basma Bakirci <144848782+besmabakirci1@users.noreply.github.com>
2024-03-14 01:36:52 +0300

    Initial commit

--- src/homework/unite3/S23.java
+++ src/homework/unite3/S23.java
@@ -1,10 +1,12 @@
 package homework.unite3;
 
+import java.util.Scanner;
+
 //(Geometry: point in a rectangle?)
 // Write a program that prompts the user to enter a point (x, y) and checks whether
 // the point is within the rectangle centered at (0, 0) with width 10 and height 5.
 // For example, (2, 2) is inside the rectangle and (6, 4) is outside the rectangle, as shown in Figure 3.7b.
-// (Hint: A point is in the rectangle if its horizontal distance to (0, 0) is less than or equal to 10 / 2
+// (Hint: A point is in the rectangle if its horizontal distance to (0, 0) is less than or equal to
 // and its vertical distance to (0, 0) is less than or equal to 5.0 / 2. Test your program to cover all cases.)
 // Here are two sample runs:
 //Enter a point with two coordinates: 2 2-> "Enter"
@@ -12,5 +14,18 @@ package homework.unite3;
 //Enter a point with two coordinates: 6 4-> "Enter"
 //Point (6.0, 4.0) is not in the rectangle
 public class S23 {
-
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a point (x,y): ");
+        double x = input.nextDouble();
+        double y = input.nextDouble();
+        double horizontalDistance = 10 / 2 ;
+        double verticalDistance = 5.0 / 2;
+        double distance = Math.sqrt(Math.pow((x),2) + Math.pow((y),2));
+        if(){
+            System.out.println("Point (" + x + ", " + y + ") is in the rectangle");
+        }else {
+            System.out.println("Point (" + x + ", " + y + ") is not in the rectangle");
+        }
+    }
 }
