cd3ee3e4bedd5e34d4b48824231dc10478e41400
Basma Bakirci <144848782+besmabakirci1@users.noreply.github.com>
2024-03-17 03:01:12 +0300

    Initial commit

--- src/homework/unite3/S28.java
+++ src/homework/unite3/S28.java
@@ -1,4 +1,22 @@
 package homework.unite3;
+//(Geometry: two rectangles)
+// Write a program that prompts the user to enter the center x-, y-coordinates, width, and height of two rectangles
+// and determines whether the second rectangle is inside the first or overlaps with the first, as shown in Figure 3.9.
+// Test your program to cover all cases.
+//Here are the sample runs:
+
+// Enter r1's center x-, y-coordinates, width, and height: 2.5 4 2.5 43-> "Enter"
+// Enter r2's center x-, y-coordinates, width, and height: 1.5 5 0.5 3-> "Enter"
+// r2 is inside r1
+
+// Enter r1's center x-, y-coordinates, width, and height: 1 2 3 5.5-> "Enter"
+// Enter r2's center x-, y-coordinates, width, and height: 3 4 4.5 5-> "Enter"
+// r2 overlaps r1
+
+// Enter r1's center x-, y-coordinates, width, and height: 1 2 3 3-> "Enter"
+// Enter r2's center x-, y-coordinates, width, and height: 40 45 3 2 -> "Enter"
+// r2 does not overlap r1
 
 public class S28 {
+
 }
--- src/homework/unite3/S32.java
+++ src/homework/unite3/S32.java
@@ -1,4 +1,23 @@
 package homework.unite3;
+// (Geometry: point position)
+// Given a directed line from point p0(x0, y0) to p1(x1, y1),
+// you can use the following condition to decide whether a point p2(x2, y2) is
+// on the left of the line,on the right, or on the same line
+// (see Figure 3.11):
+// (x1 - x0)*(y2 - y0) - (x2 - x0)*(y1 - y0) > 0 then p2 is on the left side of the line
+// (x1 - x0)*(y2 - y0) - (x2 - x0)*(y1 - y0) = 0 then p2 is on the same side of the line
+// (x1 - x0)*(y2 - y0) - (x2 - x0)*(y1 - y0) < 0 then p2 is on the right side of the line
+// Write a program that prompts the user to enter the three points for p0, p1, and p2 and displays
+// whether p2 is on the left of the line from p0 to p1, on the right, or on the same line.
+// Here are some sample runs:
 
+// Enter three points for p0, p1, and p2: 4.4 2 6.5 9.5 -5 4-> "Enter"
+// (-5.0, 4.0) is on the left side of the line from (4.4, 2.0) to (6.5, 9.5)
+
+// Enter three points for p0, p1, and p2: 1 1 5 5 2 2-> "Enter"
+// (2.0, 2.0) is on the line from (1.0, 1.0) to (5.0, 5.0)
+
+// Enter three points for p0, p1, and p2: 3.4 2 6.5 9.5 5 2.5-> "Enter"
+// (5.0, 2.5) is on the right side of the line from (3.4, 2.0) to (6.5, 9.5)
 public class S32 {
 }
