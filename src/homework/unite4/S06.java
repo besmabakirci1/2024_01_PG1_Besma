9b4adcb124ec3d4eff650d99e50023cefea55eb9
Basma Bakirci <144848782+besmabakirci1@users.noreply.github.com>
2024-03-18 16:42:14 +0300

    Initial commit

--- src/homework/unite4/S06.java
+++ src/homework/unite4/S06.java
@@ -1,4 +1,13 @@
 package homework.unite4;
+import java.util.Scanner;
+//(Random points on a circle)
+// Write a program that generates three random points on a circle centered at (0, 0) with radius 40
+// and display three angles in a triangle formed by these three points, as shown in Figure 4.7a.
+// (Hint: Generate a random angle (alfa) in radians between 0 and 2p,
+// as shown in Figure 4.7b and the point determined by this angle is (r*cos(a), r*sin(a)).)
 
 public class S06 {
+    public static void main(String[] args) {
+        //Math.random(); kullanacağım , circle center (0,0) bilgisini de kullanacağım
+    }
 }
--- src/homework/unite4/S08.java
+++ src/homework/unite4/S08.java
@@ -1,9 +1,19 @@
 package homework.unite4;
 
+import java.util.Scanner;
+
 // (Find the character of an ASCII code)
 // Write a program that receives an ASCII code (an integer between 0 and 127) and displays its character.
 // Here is a sample run:
 // Enter an ASCII code: 69-> "Enter"
 // The character for ASCII code 69 is E
 public class S08 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        System.out.print("Please enter an ASCII code: ");
+        int ASCII = input.nextInt();
+        input.close();
+        System.out.printf("The character for ASCII code",ASCII, "is " );
+//devam edilecek.
+    }
 }
