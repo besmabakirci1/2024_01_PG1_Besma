930f57b0b4449c64be389447ab23d8ecff893c27
Basma Bakirci <144848782+besmabakirci1@users.noreply.github.com>
2024-03-18 14:24:19 +0300

    Initial commit

--- src/homework/unite4/S02.java
+++ src/homework/unite4/S02.java
@@ -1,5 +1,9 @@
 package homework.unite4;
 
+import java.util.Scanner;
+
+import static java.lang.Math.*;
+
 // (Geometry: great circle distance)
 // The great circle distance is the distance between two points on the surface of a sphere.
 // Let (x1, y1) and (x2, y2) be the geographical latitude and longitude of two points.
@@ -15,4 +19,17 @@ package homework.unite4;
 // Enter point 2 (latitude and longitude) in degrees: 41.5, 87.37-> "Enter"
 // The distance between the two points is 10691.79183231593 km
 public class S02 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter the first point coordinate (x1,y1): ");
+        double x1 = Math.toRadians(input.nextDouble());
+        double y1 = Math.toRadians(input.nextDouble());
+        System.out.println("Please enter the second point coordinate (x2,y2): ");
+        double x2 = Math.toRadians(input.nextDouble());
+        double y2 = Math.toRadians(input.nextDouble());
+        input.close();
+        double radius = 6371.01;   // Earth's radius in kilometers
+        double d = radius * Math.acos(sin(x1) * sin(x2) + cos(x1) * cos(x2) * cos(y1 - y2));
+        System.out.printf("The distance between the two points is %.2f km\n", d);
+    }
 }
