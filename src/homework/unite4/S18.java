9e8d0f33ec6850ebf8cbf06743aedce095f86d22
Basma Bakirci <144848782+besmabakirci1@users.noreply.github.com>
2024-04-08 17:48:11 +0300

    Initial commit

--- src/homework/unite4/S18.java
+++ src/homework/unite4/S18.java
@@ -1,4 +1,5 @@
 package homework.unite4;
+import java.util.Scanner;
 
 // (Student major and status)
 // Write a program that prompts the user to enter two characters and displays the major and status represented in the characters.
@@ -16,4 +17,44 @@ package homework.unite4;
 // Enter two characters: T3-> "Enter"
 // Invalid input
 public class S18 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter two characters: ");
+        char major = input.next().charAt(0);
+        char status = input.next().charAt(0);
+        String majorName = "", statusName = "";
+        if (major == 'M' || major == 'C' || major == 'I' || major == 'm' || major == 'c' || major == 'i'
+                && status >= '1' && status <= '4'){
+
+            switch (major){
+                case 'M':
+                case 'm':
+                    majorName = "Mathematics";
+                    break;
+                case 'C':
+                case 'c':
+                    majorName = "Computer Science";
+                    break;
+                case 'I':
+                case 'i':
+                    majorName = "Information Technology";
+                    break;
+            }
+            switch (status){
+                case '1':
+                    statusName = "Freshman";
+                    break;
+                case '2':
+                    statusName = "Sophomore";
+                    break;
+                case '3':
+                    statusName = "Junior";
+                    break;
+                case '4':
+                    statusName = "Senior";
+                    break;
+            }
+            System.out.print(majorName + " " + statusName);
+        }else System.out.println("Invalid input ");
+    }
 }
