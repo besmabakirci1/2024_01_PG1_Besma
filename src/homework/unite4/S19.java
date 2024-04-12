b4636f7baf6c38d59263bedfa20c8bbe72f60c0a
Basma Bakirci <144848782+besmabakirci1@users.noreply.github.com>
2024-04-13 00:41:42 +0300

    Initial commit

--- src/homework/unite4/S19.java
+++ src/homework/unite4/S19.java
@@ -3,16 +3,32 @@ import java.util.Scanner;
 
 // (Business: check ISBN-10)
 // Rewrite the Programming Exercise 3.9 by entering the ISBN number as a string.
+/// Important issues from Exercise 3.9 :
+// The last digit, d10, is a checksum, which is calculated from the other nine digits using the following formula:
+// ((d1 * 1) + (d2 * 2) + (d3 * 3) + (d4 * 4) + (d5 * 5) + (d6 * 6) + (d7 * 7) + (d8 * 8) + (d9 * 9) ) % 11
+// If the checksum is 10, the last digit is denoted as X according to the ISBN-10 convention.
 public class S19 {
     public static void main(String[] args) {
         Scanner input = new Scanner (System.in);
         System.out.println("Please enter the first 9 digit of the ISBN: ");
         String firstNineDigit = input.nextLine();
-        char lastDigit = input.next().charAt(9);
-        Integer.parseInt(String.valueOf(lastDigit));
-        int lastDigitResult = (int) (firstNineDigit.charAt(0) + (firstNineDigit.charAt(1) * 2 ) + (firstNineDigit.charAt(2) * 3) + (firstNineDigit.charAt(3) * 4) + (firstNineDigit.charAt(4) * 5)
+
+        int checksum = (int) (firstNineDigit.charAt(0) + (firstNineDigit.charAt(1) * 2 ) + (firstNineDigit.charAt(2) * 3) + (firstNineDigit.charAt(3) * 4) + (firstNineDigit.charAt(4) * 5)
                 + (firstNineDigit.charAt(5) * 6) + (firstNineDigit.charAt(6) * 7) + (firstNineDigit.charAt(7) * 8)+ (firstNineDigit.charAt(8) * 9) ) % 11 ;
 
+        String ISBN = firstNineDigit;
 
+        if (checksum == 10 ){
+            ISBN += "X";
+        }else{
+            ISBN += checksum ;
+        }
+        System.out.println("The calculated ISBN: " + ISBN);
     }
 }
+
+
+
+//        Integer.parseInt(String.valueOf(lastDigit));
+//        char lastDigit = input.nextLine().charAt(9); ? burda charAt(0) olması gerektiğini ve hatanın burdan kaynaklandığını iddia etti ai ama yok asla alakası olduğuna inanmıyorum
+// 013031997 input olduğunda  0130319973 output oluyor bu da tamemen yanlış olmuş oluyor :/ diğer inputta da aynı şey oldu
--- src/homework/unite4/S21.java
+++ src/homework/unite4/S21.java
@@ -1,6 +1,8 @@
 package homework.unite4;
 import java.util.Scanner;
 
+import static java.lang.Character.isDigit;
+
 //(Check SSN)
 // Write a program that prompts the user to enter a Social Security number in the format DDD-DD-DDDD,
 // where D is a digit.Your program should check whether the input is valid.
@@ -13,7 +15,24 @@ public class S21 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Please enter the SSN (in the format DDD-DD-DDDD): ");
-        String digit = input.nextLine();
+        String ssn = input.nextLine();
 
+
+        if (ssn.length() == 11 && ssn.charAt(3) == '-' && ssn.charAt(6) == '-'){
+            String part1 = ssn.substring(0, 3);
+            String part2 = ssn.substring(4, 6);
+            String part3 = ssn.substring(7, 11);
+
+            if (isDigit(part1.charAt(0)) && isDigit(part1.charAt(1)) && isDigit(part1.charAt(2)) &&
+                    isDigit(part2.charAt(0)) && isDigit(part2.charAt(1)) &&
+                    isDigit(part3.charAt(0)) && isDigit(part3.charAt(1)) &&
+                    isDigit(part3.charAt(2)) && isDigit(part3.charAt(3))) {
+                System.out.println(ssn + " is a valid social security number");
+            } else {
+                System.out.println(ssn + " is an invalid social security number");
+            }
+        }else {
+            System.out.println(ssn + " is not in the correct format (DDD-DD-DDDD)");
+        }
     }
 }
