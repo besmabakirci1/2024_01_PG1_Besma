f8c44ab3982a1a83b99df92449c957f04a56d83e
Basma Bakirci <144848782+besmabakirci1@users.noreply.github.com>
2024-04-13 14:46:59 +0300

    Initial commit

--- src/homework/unite5/S02.java
+++ src/homework/unite5/S02.java
@@ -1,12 +1,38 @@
 package homework.unite5;
+import java.util.Scanner;
+
 // (Repeat additions)
 // Listing 5.4, SubtractionQuizLoop.java, generates five random subtraction questions.
 // Revise the program to generate ten random addition questions for two integers between 1 and 15.
 // Display the correct count and test time.
 public class S02 {
     public static void main(String[] args) {
-        final int numberOfQuestions = 10;
+        Scanner input = new Scanner(System.in);
+        final int NUMBER_OF_QUESTIONS = 10;
+        int correctCount = 0;
+        int count = 0;
+        long startTime = System.currentTimeMillis();
+        String output = " ";
 
+        while (count < NUMBER_OF_QUESTIONS) {
+            int number1 = (int) ((Math.random()) * 15) + 1;
+            int number2 = (int) ((Math.random()) * 15) + 1;
 
+            System.out.println("What is " + number1 + " + " + number2 + "? ");
+            int answer = input.nextInt();
+
+            if (number1 + number2 == answer) {
+                System.out.println("You are correct!");
+                correctCount++;
+            }else {
+                System.out.println("Your answer is wrong.\n" + number1 + " + " + number2 + " should be " + (number1 + number2));
+            }
+            count++;
+
+            long endTime = System.currentTimeMillis();
+            long testTime = endTime - startTime;
+
+            System.out.println("Correct count is " + correctCount + "\nTest time is " + testTime / 1000 + " seconds\n" + output);
+        }
     }
 }
