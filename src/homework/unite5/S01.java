6043056520dabbfe5accf08ef3473c0378cb64bd
Basma Bakirci <144848782+besmabakirci1@users.noreply.github.com>
2024-04-13 03:34:18 +0300

    Initial commit

--- src/homework/unite5/S01.java
+++ src/homework/unite5/S01.java
@@ -1,5 +1,5 @@
 package homework.unite5;
-
+import java.util.Scanner;
 // (Count positive and negative numbers and compute the average of numbers)
 // Write a program that reads an unspecified number of integers,
 // determines how many positive and negative values have been read,
@@ -14,4 +14,33 @@ package homework.unite5;
 // Enter an integer, the input ends if it is 0: 0 -> "Enter"
 // No numbers are entered except 0
 public class S01 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter the integers: ");
+
+        int positiveCount = 0;
+        int negativeCount = 0;
+        int sum = 0;
+        int number ;
+
+        while ((number = input.nextInt()) != 0){
+            if (number > 0){
+                positiveCount ++;
+            } else if (number < 0) {
+                negativeCount ++;
+            }
+            sum += number;
+        }
+
+        if (positiveCount + negativeCount > 0) {
+            double average = (double) sum / (positiveCount + negativeCount);
+            System.out.println("The number of positives is " + positiveCount);
+            System.out.println("The number of negatives is " + negativeCount);
+            System.out.println("The total is " + sum);
+            System.out.println("The average is " + average);
+        } else {
+            System.out.println("No numbers are entered except 0");
+        }
+    }
 }
+
--- src/homework/unite5/S02.java
+++ src/homework/unite5/S02.java
@@ -1,7 +1,12 @@
 package homework.unite5;
-//(Repeat additions)
+// (Repeat additions)
 // Listing 5.4, SubtractionQuizLoop.java, generates five random subtraction questions.
 // Revise the program to generate ten random addition questions for two integers between 1 and 15.
 // Display the correct count and test time.
 public class S02 {
+    public static void main(String[] args) {
+        final int numberOfQuestions = 10;
+
+
+    }
 }
