092e7687d3063558f3bc2d79bd94076f0d13d4f7
Basma Bakirci <144848782+besmabakirci1@users.noreply.github.com>
2024-04-15 03:39:12 +0300

    Initial commit

--- src/homework/unite5/S05.java
+++ src/homework/unite5/S05.java
@@ -11,30 +11,21 @@ package homework.unite5;
 
 public class S05 {
     public static void main(String[] args) {
-        System.out.println("Kilograms   \t   Pounds | Pounds    \t   Kilograms");
-        int kilograms = 1;
-        double pounds = 20;
-        while (kilograms <= 199 || pounds <= 515){
-            double poundsFromKg = kilograms * 2.2;
-            double kilogramsFromPounds = pounds * 0.4545;
-            String leftTable = (kilograms <= 199) ? String.format("%d \t %.1f", kilograms, poundsFromKg) : "    \t   ";
-            String rightTable = (pounds <= 515) ? String.format("%.1f \t %.2f", pounds, kilogramsFromPounds) : "     \t   ";
-            System.out.printf("%-15s | %s\n", leftTable, rightTable);
-            if (kilograms <= 199) kilograms += 2;
-            if (pounds <= 515) pounds += 5;
+        System.out.println("Kilograms  \t \t   Pounds  |  \t  Pounds  \t \t   Kilograms");
+
+        while (true)
+        {
+            for (int kilograms = 1; kilograms <= 199; kilograms += 2) {
+                double pound = kilograms * 2.2;
+                System.out.printf("%-3d     \t \t     %.1f   | \n", kilograms, pound);
+            }
+
+            for (double pound = 20; pound <= 515; pound += 5) {
+                 double kilograms = pound * 0.4545;
+                System.out.printf("%.1f   \t \t    %-5.2f \n", pound, kilograms);
+            }
+            break;
         }
-
-
     }
 }
-
-
-/*for (int kilograms = 1; kilograms <= 199; kilograms++){
-            double pounds = kilograms * 2.2;
-            System.out.printf("%d \t %.1f \n", kilograms , pounds);
-        }
-
-        for (double pounds = 20 ; pounds <= 515 ; pounds++ ){
-            double kilograms = pounds * 0.4545;
-            System.out.printf("%d \t %.1f \n", pounds , kilograms);
-        } */
+// yan yana nasıl yazdıracağımı çözemedim
