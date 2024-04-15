ce095349d1f87fa41d521a1aaa17524c1b71491b
Basma Bakirci <144848782+besmabakirci1@users.noreply.github.com>
2024-04-15 19:26:17 +0300

    Initial commit

--- src/homework/unite5/S07.java
+++ src/homework/unite5/S07.java
@@ -1,7 +1,11 @@
 package homework.unite5;
+
 //(Financial application: compute future tuition)
 // Suppose that the tuition for a university is $10,000 this year and increases 5% every year.
 // In one year, the tuition will be $10,500. Write a program that computes the tuition in ten years
 // and the total cost of four years’ worth of tuition after the tenth year.
 public class S07 {
+    public static void main(String[] args) {
+
+    }
 }
--- src/homework/unite5/S08.java
+++ src/homework/unite5/S08.java
@@ -1,6 +1,35 @@
 package homework.unite5;
-//(Find the highest score)
+import java.util.Scanner;
+
+// (Find the highest score)
 // Write a program that prompts the user to enter the number of students and each student’s name and score,
 // and finally displays the name of the student with the highest score.
+
 public class S08 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter the number of students:");
+        int numberOfStudents = input.nextInt();
+
+        String highestScoringStudent = "";
+        double highestScore = Double.NEGATIVE_INFINITY;
+
+        for (int i = 1; i <= numberOfStudents; i++) {
+            System.out.println("Please enter student " + i + "'s name:");
+            String studentName = input.next();
+
+            System.out.println("Please enter student " + i + "'s score:");
+            double studentScore = input.nextDouble();
+
+            if (studentScore > highestScore) {
+                highestScore = studentScore;
+                highestScoringStudent = studentName;
+            }
+        }
+        input.close();
+
+        System.out.println("The student with the highest score is: " + highestScoringStudent);
+    }
 }
+
+//         double highestScore = Double.NEGATIVE_INFINITY;  bunu anlamadım :/Diğer her şeyi yazdırmıştım ama burda tıkanınca yardım aldım
