3406c89d9d4cd28365600498786b332522dbb93b
Basma Bakirci <144848782+besmabakirci1@users.noreply.github.com>
2024-05-14 16:33:43 +0300

    Initial commit

--- src/homework/unite5/S12.java
+++ src/homework/unite5/S12.java
@@ -5,8 +5,9 @@ package homework.unite5;
 public class S12 {
     public static void main(String[] args) {
         double n = 1;
-        while (Math.pow(n,2) > 12000){
-
+        while (Math.pow(n,2) <= 12000){
+            n++;
         }
+        System.out.println("The smallest integer n such that n^2 is greater than 12,000 is: " + (int) n);
     }
 }
--- src/homework/unite5/S13.java
+++ src/homework/unite5/S13.java
@@ -3,4 +3,7 @@ package homework.unite5;
 // (Find the largest n such that Math.pow(n,3) < 12,000)
 // Use a while loop to find the largest integer n such that n^3 is less than 12,000.
 public class S13 {
+    public static void main(String[] args) {
+
+    }
 }
