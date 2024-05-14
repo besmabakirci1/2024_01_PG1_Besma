a1bdbb038a8c373bd8b088c14fdc7a03a65d1617
Basma Bakirci <144848782+besmabakirci1@users.noreply.github.com>
2024-05-14 21:30:28 +0300

    Initial commit

--- src/homework/unite5/S13.java
+++ src/homework/unite5/S13.java
@@ -4,6 +4,10 @@ package homework.unite5;
 // Use a while loop to find the largest integer n such that n^3 is less than 12,000.
 public class S13 {
     public static void main(String[] args) {
-
+        double n = 1;
+        while (Math.pow(n,3) < 12000){
+            n++;
+        }
+        System.out.println("The largest integer n such that n^3 is less than 12,000 is: " + (int) n);
     }
 }
