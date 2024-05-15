65be64506286dcb7f40bce85e24253f6d76efebb
Basma Bakirci <144848782+besmabakirci1@users.noreply.github.com>
2024-05-15 15:44:31 +0300

    Initial commit

--- src/homework/unite5/S14.java
+++ src/homework/unite5/S14.java
@@ -9,28 +9,28 @@ import java.util.Scanner;
 // Write a program that prompts the user to enter two positive integers and displays the gcd.
 public class S14 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner (System.in);
-        System.out.println("Please enter number1: ");
-        int n1 = scanner.nextInt();
-        System.out.println("Please enter number2: ");
-        int n2 = scanner.nextInt();
-        int d = Math.min(n1, n2);
-        while (d >= 1) {
-            if (n1 % d == 0 && n2 % d == 0) {
-                System.out.println("The greatest common divisor of " + n1 + " and " + n2 + " is " + d);
+        Scanner scanner = new Scanner(System.in);
+
+        int number1 = 1;
+        int number2 = 1;
+        int min = 1;
+        int gdc = 1;
+
+        System.out.print("Enter the first number: ");
+        number1 = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        number2 = scanner.nextInt();
+
+        min = Math.min(number1, number2);
+
+        for (int i = min; i > 0; i--) {
+            if ((number1 % i == 0) && (number2 % i == 0)) {
+                gdc = i;
                 break;
             }
-            d--;
         }
-
-        /* if (n1 > n2){
-            for (int i = 1 ; i < n1 ; i++){
-                if (n1 % i == n2 % i ){
-şöyle bir şeyde biraz mantıklı gibi geldi ama emin olamadım
-.
-                }
-            }
-        }
-*/
+        System.out.println("The greatest common divisor is: " + gdc);
     }
 }
+
