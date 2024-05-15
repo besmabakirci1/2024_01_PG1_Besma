982f1da411fd927af4b5f731964109f7670fbdbe
Basma Bakirci <144848782+besmabakirci1@users.noreply.github.com>
2024-05-15 19:55:33 +0300

    Initial commit

--- src/homework/unite5/S15.java
+++ src/homework/unite5/S15.java
@@ -1,11 +1,22 @@
 package homework.unite5;
 
-//(Display the ASCII character table)
+// (Display the ASCII character table)
 // Write a program that prints the characters in the ASCII character table from ! to ~.
 // Display ten characters per line. The ASCII table is shown in Appendix B.
 // Characters are separated by exactly one space
+// https://drive.google.com/file/d/1AKYepnfee9GwNTJOIuUEWkNckjWw-KHZ/view?usp=drive_link
 public class S15 {
     public static void main(String[] args) {
-
+        char start = '!' ;
+        char end = '~' ;
+        int count = 1;
+        int charactersPerLine = 10;
+        for (char ch = start; ch <= end; ch++){
+            System.out.print(ch + " ");
+            count ++;
+            if (count % charactersPerLine == 0) {
+                System.out.println();
+            }
+        }
     }
 }
--- src/homework/unite5/S16.java
+++ src/homework/unite5/S16.java
@@ -1,6 +1,22 @@
 package homework.unite5;
-//(Find the factors of an integer)
+import java.util.Scanner;
+
+// (Find the factors of an integer)
 // Write a program that reads an integer and displays all its smallest factors in increasing order.
 // For example, if the input integer is 120, the output should be as follows: 2, 2, 2, 3, 5.
 public class S16 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter an integer: ");
+        int number = scanner.nextInt();
+        for( int i = 2 ; i <= number ; i++){
+            while (number % i == 0){
+                System.out.print(i + "," + " " );
+                number /= i;
+                if(number % i != 0){
+                    break;
+                }
+            }
+        }
+    }
 }
