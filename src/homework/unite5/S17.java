0c343ad6f36794afd396c6e74cb884c9d0c69b6a
Basma Bakirci <144848782+besmabakirci1@users.noreply.github.com>
2024-05-16 13:06:19 +0300

    Initial commit

--- src/homework/unite5/S17.java
+++ src/homework/unite5/S17.java
@@ -1,14 +1,41 @@
 package homework.unite5;
-// (Display pyramid)
-// Write a program that prompts the user to enter an integer from 1 to 15 and displays a pyramid, as shown in the following sample run:
-//Enter the number of lines: 7
-//               1
-//            2  1  2
-//         3  2  1  2  3
-//      4  3  2  1  2  3  4
-//    5 4  3  2  1  2  3  4  5
-//  6 5 4  3  2  1  2  3  4  5  6
-//7 6 5 4  3  2  1  2  3  4  5  6  7
-public class S17 {
+import java.util.Scanner;
 
-}
+// (Display pyramid)
+// Write a program that prompts the user to enter an integer from 1 to 15 and displays a pyramid,
+// as shown in the following sample run:
+// Enter the number of lines: 7
+//                   1
+//                2  1  2
+//             3  2  1  2  3
+//          4  3  2  1  2  3  4
+//       5  4  3  2  1  2  3  4  5
+//    6  5  4  3  2  1  2  3  4  5  6
+// 7  6  5  4  3  2  1  2  3  4  5  6  7
+public class S17 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int line;
+        do {
+            System.out.print("Please enter a number from 1 to 15 to display lines: ");
+            line = scanner.nextInt();
+        }
+        while (line < 1 || line > 15);
+            for (int i = 1; i <= line ; i++){
+                for (int j = 0; j < line - i; j++) {
+                    System.out.print("   ");
+                }
+
+                for (int j = i; j >= 1; j--) {
+                    System.out.printf("%2d ", j);
+                }
+
+                for (int j = 2; j <= i; j++) {
+                    System.out.printf("%2d ", j);
+                }
+                System.out.println();
+            }
+        }
+    }
+
+// kendim yazamadım for kısmını :/
--- src/homework/unite5/S18.java
+++ src/homework/unite5/S18.java
@@ -1,37 +1,16 @@
 package homework.unite5;
-//(Display four patterns using loops)
+
+// (Display four patterns using loops)
 // Use nested loops that display the following patterns in four separate programs:
-//  Pattern A
-//  1
-//  12
-//  12 3
-//  123 4
-//  1234 5
-//  12345 6
-
-//  Pattern B
-//  123456
-//  12345
-//  1234
-//  123
-//  12
-//  1
-
-// Pattern C
-//       1
-//      21
-//     321
-//    4321
-//   54321
-//  654321
-
-//Pattern D
-// 123456
-// 12345
-// 1234
-// 123
-// 12
-// 1
-
+//  Pattern A           Pattern B            Pattern C                  Pattern D
+//  1                   123456                       1                  123456
+//  12                  12345                       21                  12345
+//  12 3                1234                       321                  1234
+//  123 4               123                       4321                  123
+//  1234 5              12                       54321                  12
+//  12345 6             1                       654321                  1
 public class S18 {
+    public static void main(String[] args) {
+
+    }
 }
