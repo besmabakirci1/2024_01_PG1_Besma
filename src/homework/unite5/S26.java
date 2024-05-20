4d39fc64f760efe5f2edbc7ccbfe191ab9f6b2c7
Basma Bakirci <144848782+besmabakirci1@users.noreply.github.com>
2024-05-20 18:05:37 +0300

    Initial commit

--- src/homework/unite5/S26.java
+++ src/homework/unite5/S26.java
@@ -3,8 +3,19 @@ package homework.unite5;
 //(Compute e) You can approximate e using the following series:
 // e = 1/1! + 1/2! + 1/3! + 1/4! + ... +  1/i!
 // Write a program that displays the e value for i = 10000, 20000, ..., and 100000.
-// (Hint: Because i! = i * (i - 1) * c * 2 * 1, then 1/(i!) is i*(i - 1)!
+// (Hint: Because i! = i * (i - 1) * c * 2 * 1, then 1/(i!) is i * (i - 1)!
 // Initialize e and item to be 1 and keep adding a new item to e.
 // The new item is the previous item divided by i for i = 2, 3, 4, ....)
 public class S26 {
+    public static void main(String[] args) {
+        double e = 1.0;
+        double item = 1.0;
+        for(int n = 10000; n <= 100000; n += 10000){
+            for (int i = 1; i <= n; i++){
+                item /= i;
+                e += item;
+            }
+            System.out.printf("Approximation of e for i = %d: %.15f%n", n, e);
+        }
+    }
 }
