9b3a5884a99bc98c3c5f745165f19760de8ff7b8
Basma Bakirci <144848782+besmabakirci1@users.noreply.github.com>
2024-05-25 16:21:13 +0300

    Initial commit

--- src/homework/unite5/S33.java
+++ src/homework/unite5/S33.java
@@ -7,4 +7,18 @@ package homework.unite5;
 // There are four perfect numbers less than 10,000.
 // Write a program to find all these four numbers.
 public class S33 {
+    public static void main(String[] args) {
+        System.out.println("This program will find the Perfect Numbers until 10,000 and list them all: ");
+        for (int i = 1; i <= 10000; i++) {
+            int sum = 0;
+            for (int j = 1; j < i; j++) {
+                if (i % j == 0) {
+                    sum += j;
+                }
+            }
+            if (sum == i) {
+                System.out.print(i + "  ");
+            }
+        }
+    }
 }
