402e48154533a3b6872e395f468ce8d043a34731
Basma Bakirci <144848782+besmabakirci1@users.noreply.github.com>
2024-05-26 18:33:55 +0300

    Initial commit

--- src/homework/unite5/S35.java
+++ src/homework/unite5/S35.java
@@ -1,6 +1,13 @@
 package homework.unite5;
 
-//(Summation) Write a program to compute the following summation.
+// (Summation) Write a program to compute the following summation.
 // 1 / (1 + √2) + 1 / (√2 + √3) +  1 / (√3 + √4) + ... +  1 / (√624 + √625)
 public class S35 {
+    public static void main(String[] args) {
+        double sum = 0 ;
+        for (int i = 1; i <= 624 ; i++){
+            sum +=  1/ (Math.sqrt(i) + Math.sqrt(i+1));
+        }
+        System.out.println(sum);
+    }
 }
--- src/homework/unite5/S36.java
+++ src/homework/unite5/S36.java
@@ -3,4 +3,7 @@ package homework.unite5;
 // (Business application: checking ISBN) Use loops to simplify Programming
 // Exercise 3.9.
 public class S36 {
+    public static void main(String[] args) {
+
+    }
 }
