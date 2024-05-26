9b26bb6f973cbb71af57105a2c096d695a14f38a
Basma Bakirci <144848782+besmabakirci1@users.noreply.github.com>
2024-05-27 02:04:31 +0300

    Initial commit

--- src/homework/unite5/S40.java
+++ src/homework/unite5/S40.java
@@ -3,5 +3,19 @@ package homework.unite5;
 //(Simulation: heads or tails)
 // Write a program that simulates flipping a coin one million times and displays the number of heads and tails.
 public class S40 {
-
+    public static void main(String[] args) {
+        int tailsNumber = 0 ; // yazı
+        int headsNumber = 0 ; // tura
+        int totalFlipping = 1000000;
+        for (int i = 0; i < totalFlipping; i++) {
+            int coin = (int) (Math.random() * 2);
+            if (coin == 0){
+                tailsNumber++ ;
+            }else {
+                headsNumber++ ;
+            }
+        }
+        System.out.println("Number of Tails: " + tailsNumber);
+        System.out.println("Number of Head: " + headsNumber);
+    }
 }
