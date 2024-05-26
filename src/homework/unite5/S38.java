8ce787faed32d12f7eb503fd334c6528cba99be3
Basma Bakirci <144848782+besmabakirci1@users.noreply.github.com>
2024-05-26 20:36:06 +0300

    Initial commit

--- src/homework/unite5/S38.java
+++ src/homework/unite5/S38.java
@@ -1,7 +1,27 @@
 package homework.unite5;
+import java.util.Scanner;
 
-//(Decimal to octal)
+// (Decimal to octal)
 // Write a program that prompts the user to enter a decimal integer and displays its corresponding octal value.
 // Don’t use Java’s Integer .toOctalString(int) in this program.
 public class S38 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter a decimal integer: ");
+        int decimalInteger = scanner.nextInt();
+        scanner.close();
+
+        String octalValue = "";
+        int quotient = decimalInteger; // Quotient = Bölüm
+
+        while (quotient > 0) {
+            int remainder = quotient % 8;
+            quotient = quotient / 8;
+            octalValue += remainder ;
+        }
+        for (int i = octalValue.length() - 1; i >= 0; i--) {
+            System.out.print(octalValue.charAt(i));
+        }
+
+    }
 }
