8f44d479a51be39679dec97b64b264d726400147
Basma Bakirci <144848782+besmabakirci1@users.noreply.github.com>
2024-05-26 20:00:08 +0300

    Initial commit

--- src/homework/unite5/S36.java
+++ src/homework/unite5/S36.java
@@ -3,32 +3,22 @@ import java.util.Scanner;
 
 // (Business application: checking ISBN) Use loops to simplify Programming
 // Exercise 3.9.
-//Scanner input = new Scanner (System.in);
-//        System.out.println("Please enter the first 9 digit of the ISBN as integers");
-//        int d1 = input.nextInt(), d2 = input.nextInt(), d3 = input.nextInt(),
-//            d4 = input.nextInt(), d5 = input.nextInt(), d6 = input.nextInt(),
-//            d7 = input.nextInt(), d8 = input.nextInt(), d9 = input.nextInt();
-//        int d10 = ( (d1 * 1) + (d2 * 2) + (d3 * 3) + (d4 * 4) + (d5 * 5) + (d6 * 6) + (d7 * 7) + (d8 * 8) + (d9 * 9) ) % 11 ;
-//        String isbn = String.format("%d%d%d%d%d%d%d%d", d1, d2, d3, d4, d5, d6, d7, d8, d9);
-//
-//        if (d10 == 10 ){
-//            isbn += "X";
-//        }else
-//            isbn += d10;
-//
-//        System.out.println("The ISBN-10 number is " + isbn);
+//// Here are sample runs:
+//// Enter the first 9 digits of an ISBN as integer: 013601267 -> "Enter"
+//// he ISBN-10 number is 0136012671
+//// Enter the first 9 digits of an ISBN as integer: 013031997 -> "Enter"
+//// The ISBN-10 number is 013031997X
 
 public class S36 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Please enter the first 9 digits of the ISBN as integers:");
         int sum = 0;
-        String isbn = "";
-
-        for (int i = 1; i <= 9; i++) {
-            int digit = input.nextInt();
-            isbn += digit;
-            sum += digit * i;
+        String isbnInput = input.nextLine();
+        String isbn = isbnInput;
+        for (int i = 0; i < 9; i++) {
+            int digit = Character.getNumericValue(isbnInput.charAt(i));
+            sum += digit * (i + 1);
         }
 
         int d10 = sum % 11;
@@ -39,24 +29,18 @@ public class S36 {
             isbn += d10;
         }
         System.out.println("The ISBN-10 number is " + isbn);
-
         input.close();
     }
 }
 
-//          Scanner input = new Scanner (System.in);
-//        System.out.println("Please enter the first 9 digit of the ISBN as integers");
-//
-//
-//        int d10 = ((d1 * 1) + (d2 * 2) + (d3 * 3) + (d4 * 4) + (d5 * 5) + (d6 * 6) + (d7 * 7) + (d8 * 8) + (d9 * 9)) % 11;
-//
-//        ;
-//        for (; String isbn = String.format("%d%d%d%d%d%d%d%d", d1, d2, d3, d4, d5, d6, d7, d8, d9) ; ){
-//
-//        }
-//        if (d10 == 10 ){
-//            isbn += "X";
-//        }else
-//            isbn += d10;
-//
-//        System.out.println("The ISBN-10 number is " + isbn);
+/*
+
+    for (int i = 1; i <= 9; i++) {
+            int digit = input.nextInt();
+            isbn += digit;
+            sum += digit * i;
+        }
+        yazıp yalnızca String isbn = "" ; yazdığımızda her digit sonrası enter yapmak zorunda kalıyordum o yüzden
+        Character.getNumericValue(isbnInput.charAt(i)) charAt () metodu ile i indeksindeki karakteri alıp ve değerini bulup stringi o şekilde kullanmak daha verimli oldu
+
+ */
